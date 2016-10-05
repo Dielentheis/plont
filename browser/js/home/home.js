b7cd9ace85e97a9303aa8c7256b428411decace8
Becca Clipper <bclip13@gmail.com>
2016-10-05 16:05:27 -0400

    home view functional, needs fine tweeking

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -10,10 +10,13 @@ app.controller('HomeCtrl', function ($scope, AuthService, $log) {
     AuthService.getLoggedInUser()
     .then(function (user) {
         if (!user) {
+            $scope.loggedIn = false;
             $scope.user = {};
             $scope.user.firstName = 'friend';
         } else {
+            $scope.loggedIn = true;
             $scope.user = user;
+            if (!user.firstName) $scope.user.firstName = 'friend';
         }
     })
     .catch($log.error);
