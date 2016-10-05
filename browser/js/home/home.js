4098d95a868edd835954d188e57de3664db2e6c9
Becca Clipper <bclip13@gmail.com>
2016-10-05 14:38:40 -0400

    dynamic welcome works

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,7 +1,21 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
     });
 });
+
+app.controller('HomeCtrl', function ($scope, AuthService, $log) {
+    AuthService.getLoggedInUser()
+    .then(function (user) {
+        if (!user) {
+            $scope.user = {};
+            $scope.user.firstName = 'friend';
+        } else {
+            $scope.user = user;
+        }
+    })
+    .catch($log.error);
+})
 
