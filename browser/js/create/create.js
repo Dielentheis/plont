7ed4bd287feaebd74612810afc84ef2ab5d4d1a2
Becca Clipper <bclip13@gmail.com>
2016-10-07 11:43:04 -0400

    before adding multiple-select npm package

--- browser/js/create/create.js
+++ browser/js/create/create.js
@@ -6,11 +6,33 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('CreateCtrl', function ($scope, CreatePlotFactory) {
+app.controller('CreateCtrl', function ($scope, $log, CreatePlotFactory, PlantsFactory) {
+    PlantsFactory.fetchAll()
+    .then(function (plants) {
+        $scope.plants = plants;
+    })
+    .catch($log.error);
+
+    var autoObj = function() {
+        var dataObj = {};
+        $scope.plants.forEach(function (plantObj) {
+            dataObj[plantObj.name] = null;
+        })
+        return dataObj;
+    }
+
+    $scope.autocomplete = {data: autoObj()};
+
+    // put back to false after creating add plant view
+    $scope.switch = true;
     $scope.plot = {};
     $scope.feet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     $scope.inches = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    $scope.createPlot = CreatePlotFactory.createPlot;
+    $scope.createPlot = function (hf, hi, wf, wi) {
+        CreatePlotFactory.createPlot(hf, hi, wf, wi);
+        $scope.switch = true;
+    }
+
 });
 
 app.factory('CreatePlotFactory', function () {
@@ -35,6 +57,7 @@ app.factory('CreatePlotFactory', function () {
         }
         returnObj.plot = plot;
     }
+
 
     // returnObj.renderPlot = function (plotData) {
     //     //send plot data to backend
