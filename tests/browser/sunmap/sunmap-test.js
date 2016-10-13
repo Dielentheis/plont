8c5ee3aa3babd9c8469fddbf7870aa6f56b6f67f
Becca Clipper <bclip13@gmail.com>
2016-10-13 16:19:28 -0400

    sunmap tests complete and functional

--- tests/browser/sunmap/sunmap-test.js
+++ tests/browser/sunmap/sunmap-test.js
@@ -8,22 +8,62 @@ describe('Sun Controller', function () {
         SunController = $controller('SunController', {$scope: $scope});
     }));
 
-    // describe('$scope.sunToggle', function () {
+    describe('$scope.sunToggle', function () {
 
-    //     var cell;
-    //     beforeEach(function () {
-    //         cell = {
-    //             sunniness: 'sun',
-    //             sun: 2
-    //         }
-    //     })
+        var sunCell;
+        beforeEach(function () {
+            sunCell = {
+                cells : {
+                    sunniness: 'sun',
+                    sun: 2
+                }
+            }
+        });
 
-    //     it('toggles sunniness value from sun to partial shade', function () {
-    //         $scope.sunToggle(cell.sunniness);
-    //         expect($scope.area).to.be.equal(4);
-    //     });
+        it('toggles sunniness value from sun to partial shade', function () {
+            $scope.sunToggle.call(sunCell, 'sun');
+            expect(sunCell.cells.sunniness).to.be.equal('partial_shade');
+            expect(sunCell.cells.sun).to.be.equal(1);
+        });
+        it('toggles sunniness value from partial shade to full shade', function () {
+            $scope.sunToggle.call(sunCell, 'partial_shade');
+            expect(sunCell.cells.sunniness).to.be.equal('full_shade');
+            expect(sunCell.cells.sun).to.be.equal(0);
+        });
+        it('toggles sunniness value from full shade to sun', function () {
+            $scope.sunToggle.call(sunCell, 'full_shade');
+            expect(sunCell.cells.sunniness).to.be.equal('sun');
+            expect(sunCell.cells.sun).to.be.equal(2);
+        });
 
-    // })
+    });
 
-    // to test changesTheGridSizeBackFromHalfFeetToInches, make mock PlotService.makesThenStoresThenRedirects that just returns inchesBoard.
-})
+    describe('changesTheGridSizeBackFromHalfFeetToInches', function () {
+
+        var PlotService = {};
+        beforeEach(inject(function ($controller) {
+            PlotService.makesThenStoresThenRedirects = function (board) {
+                $scope.inchesBoard = board;
+            };
+            SunController = $controller('SunController', {$scope: $scope, PlotService: PlotService});
+        }));
+
+        it('increases the dimensions of the board from half feet to inches', function () {
+            $scope.board = [[{}, {}], [{}, {}]];
+            $scope.changesTheGridSizeBackFromHalfFeetToInches();
+            expect($scope.inchesBoard.length).to.be.equal(12);
+            expect($scope.inchesBoard[0].length).to.be.equal(12);
+        });
+
+        it('can take variable board sizes', function () {
+            $scope.board = [[{}, {}, {}], [{}, {}, {}], [{}, {}, {}]];
+            $scope.changesTheGridSizeBackFromHalfFeetToInches();
+            expect($scope.inchesBoard.length).to.be.equal(18);
+            expect($scope.inchesBoard[0].length).to.be.equal(18);
+            $scope.board = [[{}, {}], [{}, {}], [{}, {}], [{}, {}]];
+            $scope.changesTheGridSizeBackFromHalfFeetToInches();
+            expect($scope.inchesBoard.length).to.be.equal(24);
+            expect($scope.inchesBoard[0].length).to.be.equal(12);
+        });
+    });
+});
