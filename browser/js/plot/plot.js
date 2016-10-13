8612871b046b3f5678d390a8a16970d1b14c237f
Dielentheis <elizabeth.dielentheis@gmail.com>
2016-10-13 15:33:32 -0400

    added number of each plant to plant in plot key

--- browser/js/plot/plot.js
+++ browser/js/plot/plot.js
@@ -17,7 +17,7 @@ app.controller('PlotCtrl', function($scope, PlotFactory, $log) {
 		$scope.plotName = plot.name;
 		$scope.plot = plot.layout;
 		$scope.data = PlotFactory.buildPlotTable(plot.layout);
-		$scope.keyObj = PlotFactory.makeKey(plot.layout, plants);
+		$scope.plantInfoArr = PlotFactory.makeKey(plot.layout, plants);
 
 	})
 	.catch($log.error);
@@ -25,7 +25,8 @@ app.controller('PlotCtrl', function($scope, PlotFactory, $log) {
 
 app.factory('PlotFactory', function($http, $stateParams, $log) {
 	var returnObj = {};
-	var idNames = {};
+	var idNames = {}; // key id, value plant name
+	var idNums = {}; // key id, value number of plant to plant
 
 	returnObj.getPlotPlants = function(plot) {
 		return $http.get('/api/plots/' + plot.id + '/plants')
@@ -41,6 +42,29 @@ app.factory('PlotFactory', function($http, $stateParams, $log) {
 		});
 	}
 
+	function calcNumsToPlant(plot, plants) {
+		for (var i = 0; i < plot.length; i++) {
+			for (var j = 0; j < plot[0].length; j++) {
+				if (!idNums[plot[i][j].plantId]) {
+					idNums[plot[i][j].plantId] = 1;
+				}
+				else {
+					idNums[plot[i][j].plantId]++;
+				}
+			}
+		}
+		divideAreaByPlants(plants);
+	}
+
+	function divideAreaByPlants(plants) {
+		plants.forEach(function(plant) {
+			if (idNums[plant.id]) {
+				var plantArea = plant.height * plant.width;
+				idNums[plant.id] = Math.floor(idNums[plant.id] / plantArea);
+			}
+		});
+	}
+
 	returnObj.fetchPlot = function() {
 		return $http.get('/api/plots/' + $stateParams.id)
 		.then(function(plot) {
@@ -51,18 +75,28 @@ app.factory('PlotFactory', function($http, $stateParams, $log) {
 
 	returnObj.makeKey = function(plot, plants) {
 		idsToNames(plants);
-		var keys = {};
+		calcNumsToPlant(plot, plants);
+		var arr = [];
 		for (var i = 0; i < plot.length; i++) {
 			for (var j = 0; j < plot[0].length; j++) {
 				if (plot[i][j].plantId) {
-					if (!keys[plot[i][j].plantId]) {
-						keys[idNames[plot[i][j].plantId]] = plot[i][j].class;
+					if (!isExisting(arr, idNames[plot[i][j].plantId])) {
+						arr.push({class: plot[i][j].class, name: idNames[plot[i][j].plantId], numToPlant: idNums[plot[i][j].plantId]})
 					}
 				}
 			}
 		}
-		return keys;
+		return arr;
 	};
+
+	function isExisting(arr, name) {
+		for (var i = 0; i < arr.length; i++) {
+			if (arr[i].name === name) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	returnObj.buildPlotTable = function(plot) {
 		var data = [];
