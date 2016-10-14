3e89a287a6172abacaf6f7a8b630a0938ef1e44e
Becca Clipper <bclip13@gmail.com>
2016-10-14 12:25:28 -0400

    Front end test2 (#75)
    * end of day 10/13
    
    * user-plant-test in progress
    
    * more tests

--- tests/browser/plants/plants-test.js
+++ tests/browser/plants/plants-test.js
@@ -46,4 +46,30 @@ describe('Plants Factory', function () {
             $httpBackend.flush();
         });
     });
+
+    describe('sortByName', function () {
+
+        var dataArr, inputOne, inputTwo;
+        beforeEach(function () {
+            dataArr = [{name: 'Watermelon'}, {name: 'Carrot'}, {name: 'Onion'}]
+            inputOne = {name: 'Basil'};
+            inputTwo = {name: 'Tomato'};
+        });
+
+        it('returns -1 if the first parameter comes alphabetically before the second', function () {
+            let result = PlantsFactory.sortByName(inputOne, inputTwo);
+            expect(result).to.be.equal(-1);
+        });
+
+        it('returns 1 if the second parameter comes alphabetically before the first', function () {
+            let result = PlantsFactory.sortByName(inputTwo, inputOne);
+            expect(result).to.be.equal(1);
+        });
+
+        it('can sort an array of objects by name property when used with .sort()', function () {
+            let result = dataArr.sort(PlantsFactory.sortByName);
+            expect(result[0].name).to.be.equal('Carrot');
+            expect(result[2].name).to.be.equal('Watermelon');
+        });
+    });
 });
---
+++ tests/browser/user-plants/user-plants-test.js
@@ -0,0 +1,49 @@
+describe('User Plant Controller', function () {
+
+    beforeEach(module('FullstackGeneratedApp'));
+
+    var $scope, UserPlantController, PlantFactory;
+    beforeEach(inject(function($controller){
+        $scope = {};
+        PlantFactory = {};
+        UserPlantController = $controller('UserPlantController', {$scope: $scope, PlantFactory: PlantFactory});
+    }));
+
+    describe('removePlant', function() {
+
+        var userId, plantId, userPlantDb, deferred;
+        beforeEach(inject(function ($q) {
+            userId = 1;
+            plantId = 1;
+            deferred = $q.defer();
+            userPlantDb = {1: [{id: 1, name: 'Basil'}, {id: 2, name: 'Carrot'}], 2: [{id: 1, name: 'Basil'}]}
+            PlantFactory.removePlantFromUser = function (uId, pId) {
+                var usersPlants = userPlantDb[uId];
+                usersPlants.forEach(function (elem, index, array) {
+                    if (elem.id === pId) {
+                        array.splice(index, 1);
+                    }
+                })
+                userPlantDb[uId] = usersPlants;
+                deferred.resolve();
+                return deferred.promise;
+            };
+            PlantFactory.fetchUserPlants = function (uId) {
+                var userPlants = userPlantDb[uId];
+                deferred.resolve(userPlants);
+                return deferred.promise;
+            };
+        }));
+
+        it('removes a plant from the users plant database', function () {
+            $scope.removePlant(1, 1);
+            expect(userPlantDb[1].length).to.be.equal(1);
+        });
+
+        it('only removes the given plant from the given user', function () {
+            $scope.removePlant(1, 1);
+            expect(userPlantDb[1].length).to.be.equal(1);
+            expect(userPlantDb[2][0].id).to.be.equal(1);
+        })
+    });
+});
