6619255e870eb5c8527a4d45a1b04465406ff3eb
tessaSAC <tessaSAC@users.noreply.github.com>
2016-10-17 01:26:44 -0400

    Resolves case-sensitive login issue and cleans up route filenames

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -13,6 +13,8 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state) {
     $scope.sendLogin = function (loginInfo) {
         $scope.error = null;
 
+        loginInfo.email = loginInfo.email.toLowerCase();
+
         AuthService.login(loginInfo)
         .then(function () {
             $state.go('home');
